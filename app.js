620aa1c5cb15bac636915472fd789e6405f8af0c
yzrsamet <yzr.samet@gmail.com>
2020-11-15 15:12:34 +0100

    added inlineHTML and textContent

--- app.js
+++ app.js
@@ -11,13 +11,18 @@
 // Array.from(titles).forEach(function(item) {
 //   console.log(item);
 // });
+//
+// var books = document.querySelector('#book-list li:nth-child(2) .name');
+// console.log(books); //tek veri dondurdu
+//
+// //Morr than 1 item; querySelectorALl();
+//
 
-var books = document.querySelector('#book-list li:nth-child(2) .name');
-console.log(books); //tek veri dondurdu
-
-//Morr than 1 item; querySelectorALl();
-
-books =  document.querySelectorAll('#book-list li .name');
-Array.from(books).forEach(function(i) {
-    console.log(i);
-});
+// books =  document.querySelectorAll('#book-list li .name');
+// Array.from(books).forEach(function(book) {
+//     book.textContent+= ' (book name)';
+// });
+//
+ // wrapper =  document.querySelector('#wrapper');
+ //  //wrapper.innerHTML = '<h1>SADECE  BEN KALDIM</h1>' bunu yoruma alinca += kullaninca sonuna ekleme yaptik.
+ //  wrapper.innerHTML += '<p>NAber</p>'
