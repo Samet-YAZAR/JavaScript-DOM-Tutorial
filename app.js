27f4627dfa5c1c347d59e0ba0dcb8a82921cb984
yzrsamet <yzr.samet@gmail.com>
2020-11-15 14:11:30 +0100

    HtmlColection and foreach loop

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-const search = document.getElementById('search-books');
-const bookList = document.getElementById('book-list');
 
-console.log(search, bookList);
+var titles = document.getElementsByClassName('title');
+
+console.log(Array.isArray(titles));
+// we can turn the HTMLCOLLECTION into array , how ?
+
+console.log(Array.isArray(Array.from(titles)));
+//we got true.
+
+Array.from(titles).forEach(function(item) {
+  console.log(item);
+});
